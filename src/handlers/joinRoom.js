379f0764cfe626ddaf4fb359212524c63077ca0d
wsl <vini.gazzana@gmail.com>
2024-09-05 16:58:15 -0300

    update:joinRoom logic

--- src/handlers/joinRoom.js
+++ src/handlers/joinRoom.js
@@ -1,13 +1,36 @@
+import redis from "../databases/redis.js";
+import { getWebSocketServer } from "../websocket.js";
+import roomsManeger from "./rooms.js";
+
 /**
  * Logic to socket join in a room
  * @param {string} roomName
  * @returns {void}
  */
-export default function joinRoom(roomName, roomPassword) {
+export default async function joinRoom(roomName, roomPassword = "") {
+  console.log(`chatroom:${roomName}`);
   const socket = this;
 
-  socket.join(roomName);
+  if (!(await roomsManeger.roomExistis(roomName))) {
+    getWebSocketServer().to(socket.id).emit("roomInexists", roomName);
+    return;
+  }
 
+  const roomInfo = await roomsManeger.roomInformations(roomName);
+  if (roomInfo.havePassword && roomPassword !== roomInfo.password) {
+    getWebSocketServer().to(socket.id).emit("wrongPassword");
+    return;
+  }
+
+  await redis.sAdd(`chatroom:${roomName}:users`, socket.id);
+
+  socket.join(roomName);
+  socket
+    .to(roomName)
+    .emit("usersJoined", { user: socket.id, when: new Date() });
+  getWebSocketServer()
+    .to(socket.id)
+    .emit("joined", { room: roomName, when: new Date() });
   console.log(
     `###### joinRoom Event ###### \nUser ${socket.id} joined room ${roomName} \n##################`
   );
