d6edd73a19d24efad0e55d1e3cc9c9bbd8267add
wsl <vini.gazzana@gmail.com>
2024-09-25 21:54:27 -0300

    feat: login route

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,5 +1,5 @@
 import prisma from "../databases/prisma.js";
-import { hashPassword } from "../utils/crypto.js";
+import { compareHash, hashPassword } from "../utils/crypto.js";
 
 export default async function AuthenticationRoutes(fastify) {
   fastify.post("/register", async (request, reply) => {
@@ -47,6 +47,57 @@ export default async function AuthenticationRoutes(fastify) {
         status: "error",
         message: error.message,
         error,
+      };
+    }
+  });
+
+  fastify.post("/login", async (request, reply) => {
+    try {
+      const { data } = request.body;
+
+      const { email, password } = data;
+
+      const user = await prisma.user.findFirst({
+        where: { email },
+      });
+
+      if (!user) {
+        return reply.status(404).send({
+          status: "warning",
+          message: "User not found!",
+          data: null,
+        });
+      }
+
+      const isPasswordValid = compareHash(
+        hashPassword(password),
+        user.password
+      );
+
+      if (!isPasswordValid) {
+        return reply.status(401).send({
+          status: "warning",
+          message: "Invalid password!",
+          data: null,
+        });
+      }
+
+      return {
+        error: false,
+        data: {
+          user: {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+          },
+        },
+      };
+    } catch (error) {
+      return {
+        error: true,
+        message: error.message,
+        errorObj: { ...error },
+        data: null,
       };
     }
   });
